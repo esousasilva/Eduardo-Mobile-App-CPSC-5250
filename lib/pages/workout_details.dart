3ee17c6c994d3f2fb989e7d1089c4bf86c573a60
esousasilva <esousasilva@seattleu.edu>
2025-02-13 16:09:52 -0800

    Added UserPerformanceWidget to workout_details page

--- lib/pages/workout_details.dart
+++ lib/pages/workout_details.dart
@@ -1,5 +1,8 @@
+import 'package:eduardo_personal_app/pages/user_performance_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../model/exercise_result.dart';
+import '../model/workout_plan.dart';
 
 class WorkoutDetails extends StatelessWidget {
   final List<ExerciseResult> exerciseResult;
@@ -8,32 +11,40 @@ class WorkoutDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final workoutExercises = context.watch<WorkoutPlan>();
     return Scaffold(
       appBar: AppBar(title: Text('Workout History')),
       body: SafeArea(
-        child: exerciseResult.isNotEmpty ? ListView.builder(
-          itemCount: exerciseResult.length,
-          itemBuilder: (context, index) {
-            final result = exerciseResult[index];
-            final _valueAchieved = (result.actuallyAchievedOutput*100/result.exercise.target_output)/100;
-            return ListTile(
-              title: Text('Exercise Name: ${result.exercise.name}'),
-              subtitle:
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                      'Target: ${result.exercise.target_output} ${result.exercise.unit.label} \n'
-                          'Actual output: ${result.actuallyAchievedOutput} ${result.exercise.unit.label}'
+        child: Column(
+          children: [
+            UserPerformanceWidget(),
+            SizedBox(height: 8,),
+            exerciseResult.isNotEmpty ? ListView.builder(
+            shrinkWrap: true,
+            itemCount: exerciseResult.length,
+            itemBuilder: (context, index) {
+              final result = exerciseResult[index];
+              final _valueAchieved = (result.actuallyAchievedOutput*100/result.exercise.target_output)/100;
+              return ListTile(
+                  title: Text('Exercise Name: ${result.exercise.name}'),
+                  subtitle:
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                            'Target: ${result.exercise.target_output} ${result.exercise.unit.label} \n'
+                                'Actual output: ${result.actuallyAchievedOutput} ${result.exercise.unit.label}'
 
-                  ),
-                  SizedBox(height: 5),
-                ]
-              )
-            );
-          },
+                        ),
+                        SizedBox(height: 5),
+                      ]
+                  )
+              );
+            },
+          )
+              : Center(child: Text('No workout history Available')),
+          ],
         )
-            : Center(child: Text('No workout history Available')),
       ),
     );
   }
