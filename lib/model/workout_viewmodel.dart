218bd03e647996f4799b592dca4f7410672d7744
esousasilva <esousasilva@seattleu.edu>
2025-02-18 10:29:44 -0800

    Added database logic to add and load workouts at workout_viewmodel.dart file

--- lib/model/workout_viewmodel.dart
+++ lib/model/workout_viewmodel.dart
@@ -2,22 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:eduardo_personal_app/model/exercise.dart';
 import 'package:eduardo_personal_app/model/workout.dart';
 import 'package:eduardo_personal_app/model/exercise_result.dart';
+import 'package:eduardo_personal_app/services/database_service.dart';
 
 class WorkoutViewModel extends ChangeNotifier {
   List<Workout> _workoutHistory = [];
   List<Exercise> _exercises = [
-    Exercise('Squats', ExerciseType.repetitions),
-    Exercise('Jogging', ExerciseType.miles),
-    Exercise('Bench Press', ExerciseType.seconds),
-    Exercise('Swimming', ExerciseType.laps),
+    Exercise('Squats', 0, ExerciseType.repetitions),
+    Exercise('Jogging', 0, ExerciseType.miles),
+    Exercise('Bench Press', 0, ExerciseType.seconds),
+    Exercise('Swimming', 0, ExerciseType.laps),
   ];
 
   List<Workout> get workoutHistory => _workoutHistory;
   List<Exercise> get exercises => _exercises;
 
-  void addWorkout(String name, List<ExerciseResult> results) {
-    final workout = Workout(name, results, DateTime.now());
+  final DatabaseHelper _dbHelper = DatabaseHelper.instance;
+
+
+  // Load saved workouts from the database
+  Future<void> loadWorkouts() async {
+    _workoutHistory = await _dbHelper.getWorkouts();
+    notifyListeners();
+  }
+
+  // Add workout and save to SQLite
+  Future<void> addWorkout(String name, List<ExerciseResult> results) async {
+    final workout = Workout(name, results, DateTime.now()); // Pass results directly
+
     _workoutHistory.add(workout);
+    await _dbHelper.insertWorkout(workout);  // Save to SQLite
     notifyListeners();
   }
 }
