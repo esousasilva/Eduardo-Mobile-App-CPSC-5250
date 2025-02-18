aee4303f5ed993ad08efbbef94849b3d94a16413
esousasilva <esousasilva@seattleu.edu>
2025-02-18 10:25:35 -0800

    Added convertion for database at exercise.dart and exercise_result.dart files

--- lib/model/exercise.dart
+++ lib/model/exercise.dart
@@ -1,18 +1,45 @@
-class Exercise {
-  final String name;
-  late int target_output;
-  final ExerciseType unit;
-
-  Exercise(this.name, this.unit);
-}
+import 'dart:convert';
 
 enum ExerciseType {
   seconds('Seconds'),
   repetitions('Repetitions'),
   miles('Miles'),
   laps('Laps'),
-  jumps('Jumps');
+  jumps('Jumps'),
+  meters('Meters');
 
   const ExerciseType(this.label);
   final String label;
+}
+
+class Exercise {
+  final String name;
+  late int target_output;
+  final ExerciseType unit;
+
+  Exercise(this.name, this.target_output, this.unit);
+
+  // Convert Exercise to a Map (for sqflite)
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'target': target_output,
+      'unit': unit.name, // Store as string
+    };
+  }
+
+  // Convert a Map to an Exercise object
+  factory Exercise.fromMap(Map<String, dynamic> map) {
+    return Exercise(
+      map['name'],
+      map['target'],
+      ExerciseType.values.firstWhere((e) => e.name == map['unit']),
+    );
+  }
+
+  // Convert Exercise to JSON
+  String toJson() => json.encode(toMap());
+
+  // Convert JSON to Exercise
+  factory Exercise.fromJson(String source) => Exercise.fromMap(json.decode(source));
 }
--- lib/model/exercise_result.dart
+++ lib/model/exercise_result.dart
@@ -1,8 +1,24 @@
-import 'package:eduardo_personal_app/model/exercise.dart';
+import 'exercise.dart';
 
 class ExerciseResult {
   final Exercise exercise;
-  final int actuallyAchievedOutput;
+  final int achievedOutput;
 
-  const ExerciseResult(this.exercise, this.actuallyAchievedOutput);
+  ExerciseResult(this.exercise, this.achievedOutput);
+
+  // Convert ExerciseResult to a Map for storing in SQLite
+  Map<String, dynamic> toMap() {
+    return {
+      'exercise': exercise.toMap(), // Convert Exercise to Map
+      'achievedOutput': achievedOutput,
+    };
+  }
+
+  // Convert a Map back to ExerciseResult
+  factory ExerciseResult.fromMap(Map<String, dynamic> map) {
+    return ExerciseResult(
+      Exercise.fromMap(map['exercise']), // Convert back to Exercise
+      map['achievedOutput'],
+    );
+  }
 }
