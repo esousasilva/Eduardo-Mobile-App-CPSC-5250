25be26b85bc08229d29147b737f832658858d14f
esousasilva <esousasilva@seattleu.edu>
2025-02-19 18:27:05 -0800

    Updated workout.dart file to convert workout from json to an object

--- lib/model/workout.dart
+++ lib/model/workout.dart
@@ -1,11 +1,32 @@
+import 'dart:convert';
+
 import 'package:eduardo_personal_app/model/exercise_result.dart';
 
 class Workout {
   final String name;
-
   final List<ExerciseResult> exerciseResults;
-
   final DateTime dateTimeWhenWasDone;
+  final bool isDownloaded;
 
-  Workout(this.name, this.exerciseResults, this.dateTimeWhenWasDone);
+  Workout(this.name, this.exerciseResults, this.dateTimeWhenWasDone, {this.isDownloaded = false});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'exercises': json.encode(exerciseResults.map((e) => e.toMap()).toList()),
+      'dateTimeWhenWasDone': dateTimeWhenWasDone.toIso8601String(),
+      'isDownloaded': isDownloaded ? 1 : 0,  // Store as integer in database
+    };
+  }
+
+  factory Workout.fromMap(Map<String, dynamic> map) {
+    return Workout(
+      map['name'],
+      List<ExerciseResult>.from(
+        json.decode(map['exercises']).map((e) => ExerciseResult.fromMap(e)),
+      ),
+      DateTime.parse(map['dateTimeWhenWasDone']),
+      isDownloaded: map['isDownloaded'] == 1,  // Convert from integer
+    );
+  }
 }
