6da941a48b18121d04b8a36b094bdd5ad4c2bd3b
esousasilva <esousasilva@seattleu.edu>
2025-03-03 18:23:16 -0800

    Features added to workout_history_page.dart file: delete function, add the workout name and go_router navigation

--- lib/pages/workout_history_page.dart
+++ lib/pages/workout_history_page.dart
@@ -3,8 +3,8 @@ import 'package:eduardo_personal_app/pages/user_performance_widget.dart';
 import 'package:eduardo_personal_app/pages/workout_recording_page.dart';
 import 'package:flutter/material.dart';
 import 'package:eduardo_personal_app/pages/workout_details.dart';
+import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
-
 
 class WorkoutHistoryPage extends StatelessWidget {
   const WorkoutHistoryPage({super.key});
@@ -20,23 +20,37 @@ class WorkoutHistoryPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             UserPerformanceWidget(),
-            SizedBox(height: 10,),
+            SizedBox(
+              height: 10,
+            ),
             ListView.builder(
               shrinkWrap: true,
               itemCount: workoutViewModel.workoutHistory.length,
               itemBuilder: (context, index) {
                 final workout = workoutViewModel.workoutHistory[index];
-                return ListTile(
-                  title: Text(workout.name),
-                  subtitle: Text('Date: ${workout.dateTimeWhenWasDone}'),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => WorkoutDetails(exerciseResult: workout.exerciseResults),
+                return Container(
+                  width: double.infinity, // Ensure it takes full width
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: ListTile(
+                          title: Text('Name:${workout.name}, ID: ${workout.id}'),
+                          subtitle: Text('Date: ${workout.dateTimeWhenWasDone}'),
+                          onTap: () {
+                            context.pushNamed('/workout_detail', extra: workout.exerciseResults);
+                          },
+                        ),
                       ),
-                    );
-                  },
+                      ElevatedButton(
+                          onPressed: (){
+                            workoutViewModel.deleteWorkout(workout.id);
+                          },
+                          child: Icon(
+                            Icons.delete,
+                          )
+                      )
+                    ],
+                  ),
                 );
               },
             ),
@@ -44,10 +58,8 @@ class WorkoutHistoryPage extends StatelessWidget {
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 8.0),
               child: ElevatedButton(
-                onPressed: () async {
-                  await Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => const WorkoutRecordingPage()),
-                  );
+                onPressed: () {
+                  context.pushNamed('/start_workout');
                 },
                 style: ElevatedButton.styleFrom(
                   padding: EdgeInsets.symmetric(vertical: 15),
