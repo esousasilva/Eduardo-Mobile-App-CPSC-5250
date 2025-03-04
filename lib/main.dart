e5c32a10b96260d5608c7c5720baad40aaf42253
esousasilva <esousasilva@seattleu.edu>
2025-03-03 18:06:48 -0800

    Changed main.dart file to use go_router

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:eduardo_personal_app/services/app_routes.dart';
+import 'package:eduardo_personal_app/services/database_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:eduardo_personal_app/pages/home_screen.dart';
@@ -5,6 +7,7 @@ import 'package:eduardo_personal_app/model/workout_viewmodel.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  //DatabaseHelper.instance.deleteDatabaseFile();
 
   runApp(
     MultiProvider(
@@ -21,10 +24,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
-      title: 'Workout Tracker',
-      home: HomeScreen(),
     );
   }
 }
