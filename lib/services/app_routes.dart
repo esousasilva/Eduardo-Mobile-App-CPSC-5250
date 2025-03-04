cc4ff1cf53a326907832831def2c619498ce2b40
esousasilva <esousasilva@seattleu.edu>
2025-03-03 18:05:41 -0800

    Added goroute navigation to the project

---
+++ lib/services/app_routes.dart
@@ -0,0 +1,48 @@
+import 'package:eduardo_personal_app/model/exercise_result.dart';
+import 'package:eduardo_personal_app/pages/workout_details.dart';
+import 'package:eduardo_personal_app/pages/workout_history_page.dart';
+import 'package:eduardo_personal_app/pages/workout_import_page.dart';
+import 'package:eduardo_personal_app/pages/workout_recording_page.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:eduardo_personal_app/pages/home_screen.dart';
+
+final GoRouter appRouter = GoRouter(
+  initialLocation: '/history',
+  routes: [
+    ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(child: child);
+      },
+      routes: [
+        GoRoute(
+          path: '/history',
+          builder: (context, state) => WorkoutHistoryPage(),
+        ),
+        GoRoute(
+          path: '/import',
+          builder: (context, state) => WorkoutImportPage(),
+        ),
+        GoRoute(
+          path: '/user',
+          builder: (context, state) => Placeholder(),
+        )
+      ]
+    ),
+    GoRoute(
+      name: '/workout_detail',
+      path: '/workout_detail',
+      builder: (context, state) {
+        final List<ExerciseResult> exerciseResult  = state.extra as List<ExerciseResult>;
+        return WorkoutDetails(exerciseResult: exerciseResult);
+      }
+    ),
+    GoRoute(
+      name: "/start_workout",
+      path: "/start_workout",
+      builder: (context, state) {
+        return WorkoutRecordingPage();
+      }
+    ),
+  ]
+);
