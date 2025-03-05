6171eb06d4a4297677aede32993d8774dbc9b378
esousasilva <esousasilva@seattleu.edu>
2025-03-04 21:05:20 -0800

    Changed main to initialize firebase database

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:eduardo_personal_app/services/app_routes.dart';
-import 'package:eduardo_personal_app/services/database_service.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:eduardo_personal_app/pages/home_screen.dart';
 import 'package:eduardo_personal_app/model/workout_viewmodel.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await _initializeFirebase();
   //DatabaseHelper.instance.deleteDatabaseFile();
 
   runApp(
@@ -16,6 +17,12 @@ void main() async {
       ],
       child: MyApp(),
     ),
+  );
+}
+
+Future<void> _initializeFirebase() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
 }
 
